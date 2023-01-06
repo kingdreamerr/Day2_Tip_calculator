da494a643a5f23f3ea49b9ef5ae4ec2532e9aedf
kingdreamerr <jackmvula458@gmail.com>
2023-01-06 22:43:17 +0200

    Add actual price of the bill

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 print("Welcome to tip calculator!")
-bill = ("What was the bill? ")
-tip = ("What percentage tip would you like to give 10, 12, or 15? ")
+bill = float(input("What was the bill? "))
+tip = float(input("What percentage tip would you like to give 10, 12, or 15? "))
+people = int(input("How many people to split the bill? "))
+
+# find the price of the tip 
+tip_price = (tip /100) * bill
+print(tip_price)
